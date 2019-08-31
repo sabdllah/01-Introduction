94219b49f415bebd1aee572605eeb01d4ff82111
Safiyyah Abd'Llah <sabdllah@iu.edu>
2019-08-31 18:04:52 -0400

    Completed Assignment and made changes.

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ utils.check_version((3,7))
 utils.clear()
 
 print('Hello, my name is Safiyyah AbdLlah')
-print('')
+print('my favourite game is Mario')
+print ('i am conncerned that it will be too fast paced for me')
+print ('i am excited to make games')
+stack_id = '12004299'
+print ('my stackopverflow user number = ',stack_id)
+print ('my github profile URL is https://github.com/sabdllah')
